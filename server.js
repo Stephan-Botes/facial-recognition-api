da554a7b96f97bd11c801a5c81f159bc2141e62c
Stephan-Botes <stephan.botes1@gmail.com>
2021-07-22 17:45:40 +0200

    Added server response

--- server.js
+++ server.js
@@ -24,28 +24,28 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send(postgresDB.users);
-})
+    res.send('The facial-recognition-api is running.')
+});
 
 app.post('/signin', signin.handleSignin(postgresDB, bcrypt))
 
 app.post('/register', (req, res) => {
     register.handleRegister(req, res, postgresDB, bcrypt);
-})
+});
 
 app.get('/profile/:id', (req, res) => {
     profile.handleProfileGet(req, res, postgresDB);
-})
+});
 
 app.post('/imageurl', (req, res) => {
     image.handleApiCall(req, res);
-})
+});
 
 app.put('/image', (req, res) => {
     image.handleImage(req, res, postgresDB);
-})
+});
 
 // Server start on port 8000
 app.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
-})
+});
